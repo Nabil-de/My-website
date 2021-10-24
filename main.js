8245fe9867e4a1334807d1b7c238a324677db028
Nabil <91741203+Nabil-de@users.noreply.github.com>
2021-10-24 16:01:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ gsap.from('.title', { opacity: 0, duration: 3, delay: 3.2, y: 25, ease: 'expo.ou
 gsap.from('.mail', { opacity: 0, duration: 3, delay: 2.3, y: 25, ease: 'expo.out', stagger: .3 })
 
 
-gsap.from('.ani', { opacity: 0, duration: 2, delay: 2, x: 60 })
+
 
 
 
